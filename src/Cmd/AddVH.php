dfe9c0453906be1e5adb80a74aa5015904cc72e4
ahmed adel <abo3adel35@gmail.com>
2019-09-07 21:35:53 +0200

    handle all options

---
+++ src/Cmd/AddVH.php
@@ -0,0 +1,44 @@
+<?php declare(strict_types=1);
+
+namespace AboAdel\VHost\Cmd;
+
+use AboAdel\VHost\FileHandler;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputDefinition;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class AddVH extends Command
+{
+    protected static $defaultName = 'xamp-vh:addHost';
+
+    protected function configure() : void
+    {
+        $this
+            ->setDescription('Add new Virtual Host')
+            ->setHelp('this command for adding new virtual host')
+            ->addOption('host', '-h', InputOption::VALUE_REQUIRED, 'Host name')
+            ->addOption('dir', '-d', InputOption::VALUE_OPTIONAL, 'Host Directory')
+            ->addOption('admin', '-a', InputOption::VALUE_OPTIONAL)
+            ->addOption('alias', '-as', InputOption::VALUE_OPTIONAL)
+            ->addOption('error-log', '-e', InputOption::VALUE_OPTIONAL)
+            ->addOption('custom-log', 'cl', InputOption::VALUE_OPTIONAL);
+    }
+
+    
+
+    protected function execute(
+        InputInterface $input,
+        OutputInterface $output
+    ) : void {
+        $output->writeln([
+            'User Creator',
+            '============',
+            '',
+        ]);
+        $output->writeln($input->getOption('interactive'));
+    }
+}
+
