ab0772cf926cfb55a3191fd39c6d8fb85bc15908
ahmed adel <abo3adel35@gmail.com>
2019-09-08 03:51:40 +0200

    remove unused use statement

--- src/Cmd/AddVH.php
+++ src/Cmd/AddVH.php
@@ -2,11 +2,6 @@
 
 namespace AboAdel\VHost\Cmd;
 
-use AboAdel\VHost\FileHandler;
-use Exception;
-use Symfony\Component\Console\Command\Command;
-use Symfony\Component\Console\Input\InputArgument;
-use Symfony\Component\Console\Input\InputDefinition;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
--- src/Cmd/Interactive.php
+++ src/Cmd/Interactive.php
@@ -2,13 +2,8 @@
 
 namespace AboAdel\VHost\Cmd;
 
-use AboAdel\VHost\FileHandler;
 use RuntimeException;
-use Symfony\Component\Console\Command\Command;
-use Symfony\Component\Console\Input\InputArgument;
-use Symfony\Component\Console\Input\InputDefinition;
 use Symfony\Component\Console\Input\InputInterface;
-use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Style\SymfonyStyle;
 
